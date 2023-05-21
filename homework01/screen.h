e77d42a8b07dfd6aaa5b6878c29ad84d2ee1e071
wchan2004 <wchan2004@naver.com>
2023-05-21 21:53:46 +0900

    screen.h

--- homework01/screen.h
+++ homework01/screen.h
@@ -1,4 +1,5 @@
-int explain();
-int menu();
-int main();
-
+int title(char* screen, int width, int height);
+int menu(char* screen, int width, int height);
+int explanation(char* screen, int width, int height);
+int finish(char* screen, int width, int height);
+int writeString(const char* string, char* screen, int width, int x, int y);
