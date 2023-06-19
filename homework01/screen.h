a304aef1ff4c33d65a28fd7b6cb688e36618152a
wchan2004 <132328647+wchan2004@users.noreply.github.com>
2023-06-19 23:17:14 +0900

    Delete screen.h

--- homework01/screen.h
+++
@@ -1,5 +0,0 @@
-int title(char* screen, int width, int height);
-int menu(char* screen, int width, int height);
-int explanation(char* screen, int width, int height);
-int finish(char* screen, int width, int height);
-int writeString(const char* string, char* screen, int width, int x, int y);
